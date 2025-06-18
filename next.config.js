698bfc2ec7ebd2e38a7677a6263f69abc8cd4782
isaacharlem <isaacharlem@uchicago.edu>
2025-06-18 15:45:26 -0700

    optional gemini api key

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY || '',
   }
 }
 
